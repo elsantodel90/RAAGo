b7395cea950ac123ead75fa5913de2dd15f53834
Ignacio Rossi <rossi.ignacio@gmail.com>
2016-06-25 18:48:18 -0300

    Add admin for player and games

--- aago_ranking/games/admin.py
+++ aago_ranking/games/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Player, Game
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'black_player', 'white_player', 'handicap', 'komi',
+                    'result', 'win_reason', 'points')
