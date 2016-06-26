5def8abf70cd4ebc5e2a3bfe1da1f1d96856fc9f
Ignacio Rossi <rossi.ignacio@gmail.com>
2016-06-25 23:48:56 -0300

    Simple admin for ratings

--- aago_ranking/ratings/admin.py
+++ aago_ranking/ratings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PlayerRating
+
+
+@admin.register(PlayerRating)
+class PlayerRatingAdmin(admin.ModelAdmin):
+    list_display = ('event', 'player', 'mu', 'sigma')
