4ec6b93706442f9d6d79c7c5902513dd8a0171de
Ignacio Rossi <rossi.ignacio@gmail.com>
2016-06-29 22:56:17 -0300

    Achicar las textareas del ABM de eventos

--- aago_ranking/events/admin.py
+++ aago_ranking/events/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.forms.widgets import Textarea
+from django.db.models import TextField
 
 from aago_ranking.games.models import Game
 
@@ -15,12 +17,18 @@ class EventPlayerInline(admin.TabularInline):
 class EventGameInline(admin.TabularInline):
     model = Game
     extra = 1
+    formfield_overrides = {
+        TextField: {'widget': Textarea(attrs={'rows': 1})}
+    }
 
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display = ('name', 'start_date', 'end_date')
     inlines = (EventPlayerInline, EventGameInline)
+    formfield_overrides = {
+        TextField: {'widget': Textarea(attrs={'rows': 2})}
+    }
 
 
 @admin.register(EventPlayer)
