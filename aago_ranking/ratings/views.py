3dfda916605433074de0a48cb32f50711bdcba10
Ignacio Rossi <rossi.ignacio@gmail.com>
2016-07-06 15:46:57 -0300

    Require staff login to run the rating update

--- aago_ranking/ratings/views.py
+++ aago_ranking/ratings/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.contrib.admin.views.decorators import staff_member_required
 from django.http import Http404
 from django.http import JsonResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 from . import tasks
 
+@staff_member_required
 def runRatingsUpdates(request):
     if request.method == "POST":
         json = tasks.run_ratings_update()
