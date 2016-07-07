68a92c547b1f007b67f1a846c816658c628078ad
Ignacio Rossi <rossi.ignacio@gmail.com>
2016-07-06 21:52:32 -0300

    Traerse los jugadores con la query de los ratings

--- aago_ranking/web/views.py
+++ aago_ranking/web/views.py
@@ -4,7 +4,7 @@ from aago_ranking.ratings.models import PlayerRating
 
 
 def homepage(request):
-    ratings = PlayerRating.objects.order_by('event')
+    ratings = PlayerRating.objects.order_by('event').select_related('player')
     last_ratings = {r.player: r.mu for r in ratings}
     sorted_ratings = sorted(last_ratings.items(), reverse=True, key=(lambda item: item[1]))
     return render(request, 'pages/home.html', {'sorted_ratings': sorted_ratings, })
