c933a0e361ccf359e54f607748b39fdcee0db691
Ignacio Rossi <rossi.ignacio@gmail.com>
2016-07-06 21:25:26 -0300

    Reemplazar el form para actualizar los ratings por un link

--- aago_ranking/ratings/urls.py
+++ aago_ranking/ratings/urls.py
@@ -2,4 +2,4 @@ from django.conf.urls import url
 
 from . import views
 
-urlpatterns = [url(r'run-ratings-update', views.run_ratings_updates, name='run_ratings_update'), ]
+urlpatterns = [url(r'run-ratings-update', views.run_ratings_update, name='run_ratings_update'), ]
--- aago_ranking/ratings/views.py
+++ aago_ranking/ratings/views.py
@@ -1,14 +1,10 @@
 from django.contrib.admin.views.decorators import staff_member_required
-from django.http import Http404
 from django.http import JsonResponse
 
 from . import tasks
 
 
 @staff_member_required
-def run_ratings_updates(request):
-    if request.method == "POST":
-        json = tasks.run_ratings_update()
-        return JsonResponse(json)
-    else:
-        raise Http404()  # Meh, seria 405
+def run_ratings_update(_request):
+    json = tasks.run_ratings_update()
+    return JsonResponse(json)
