22b9d79146c4faf95d5642cdc21cdf1ebb2fea69
Ignacio Rossi <rossi.ignacio@gmail.com>
2016-07-06 21:51:11 -0300

    Limpiar código y template de la home

--- aago_ranking/web/views.py
+++ aago_ranking/web/views.py
@@ -1,17 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 from aago_ranking.ratings.models import PlayerRating
 
-def homepage(request):
-    # TemplateView.as_view(template_name='pages/home.html')
-    allHistoryRatings = PlayerRating.objects.order_by('event')
-    playerRatings = dict()
-    for rating in allHistoryRatings:
-        playerRatings[rating.player.name] = rating.mu
-    sortedRatings = sorted(playerRatings.items(), reverse=True, key=(lambda item : item[1]))
-    players = [{"ranking" : i+1, "rating" : "{:.3f}".format(mu), "name" : name} for i, (name, mu) in enumerate(sortedRatings)]
-    return render(request, 'pages/home.html', {
-        'players': players, #[{ "ranking" : 1, "rating" : 1.521, "name" : "Rodrigo Batata" }, ],
-    })
 
+def homepage(request):
+    ratings = PlayerRating.objects.order_by('event')
+    last_ratings = {r.player: r.mu for r in ratings}
+    sorted_ratings = sorted(last_ratings.items(), reverse=True, key=(lambda item: item[1]))
+    return render(request, 'pages/home.html', {'sorted_ratings': sorted_ratings, })
