7dc963b08a1dcce9176a13d14b76ad925b8f26bd
Ignacio Rossi <rossi.ignacio@gmail.com>
2016-07-06 21:43:06 -0300

    Mover las views home y about a su propia app

--- scripts/run_pylint.sh
+++ scripts/run_pylint.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -u
 
-for app in games events ratings; do
+for app in games events ratings web; do
   echo "Checking app $app with pylint"
   pylint aago_ranking/$app -r n --ignore=migrations
 done
