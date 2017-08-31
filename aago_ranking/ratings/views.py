8889bc8c68f93e0b450742992c341f6e6debdf79
Agustín Santiago Gutiérrez <elsantodel90@gmail.com>
2017-08-31 19:08:21 -0300

    Se exige metodo post para ejecutar un rating update (mucho mas prolijo)

--- aago_ranking/ratings/views.py
+++ aago_ranking/ratings/views.py
@@ -1,10 +1,12 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
 
 from . import tasks
 
 
 @staff_member_required
+@require_http_methods(["POST"])
 def run_ratings_update(_request):
     json = tasks.run_ratings_update()
     return JsonResponse(json)
